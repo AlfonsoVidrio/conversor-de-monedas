d859c265b9af8a87c8e82ff1f944a97f8f3856bb
Alfonso Vidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2024-09-26 13:53:49 -0600

    Add .env file and dotenv-java dependency

--- src/main/java/application/App.java
+++ src/main/java/application/App.java
@@ -1,7 +1,11 @@
 package application;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Dotenv dotenv = Dotenv.load();
+        String value = dotenv.get("MI_VARIABLE");
+        System.out.println("El valor de MI_VARIABLE es: " + value);
     }
 }
